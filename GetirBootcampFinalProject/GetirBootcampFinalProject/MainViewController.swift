3de39f84ce1c8401ca832e936e7d8f02f86ec199
selmanadanir <adanirselman@gmail.com>
2024-04-10 19:19:17 +0300

    configure project to programmatically without storyboard

---
+++ GetirBootcampFinalProject/GetirBootcampFinalProject/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GetirBootcampFinalProject
+//
+//  Created by Selman Adanir on 10.04.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
