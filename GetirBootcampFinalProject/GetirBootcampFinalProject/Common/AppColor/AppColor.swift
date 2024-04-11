8dde109db2cca8252187fe6e13d930ca0c1eb00a
selmanadanir <adanirselman@gmail.com>
2024-04-11 11:02:04 +0300

    create color set in assets and AppColor for uicolor

---
+++ GetirBootcampFinalProject/GetirBootcampFinalProject/Common/AppColor/AppColor.swift
@@ -0,0 +1,22 @@
+//
+//  AppFont.swift
+//  GetirBootcampFinalProject
+//
+//  Created by Selman Adanir on 11.04.2024.
+//
+
+import UIKit
+
+struct AppColor {
+    enum Name: String {
+        case body = "bodyColorSet"
+        case head = "headColorSet"
+        case white = "whiteColorSet"
+        case primary = "primaryColorSet"
+        case background = "backgroundColorSet"
+    }
+    
+    static func getColor(_ name: Name) -> UIColor? {
+        return UIColor(named: name.rawValue)
+    }
+}
--- GetirBootcampFinalProject/GetirBootcampFinalProject/Module/ProductListingScreen/ProductListingViewController.swift
+++ GetirBootcampFinalProject/GetirBootcampFinalProject/Module/ProductListingScreen/ProductListingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class ProductListingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
